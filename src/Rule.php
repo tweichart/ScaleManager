b58aa3f5120205e73588b9aa5aa71c089028e4e8
nibra <nbraczek@bsds.de>
2017-03-26 13:42:05 +0200

    Rule

---
+++ src/Rule.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Part of the InterNetX ScaleManager
+ *
+ * @copyright  Copyright (C) 2017 InterNetX GmbH. All rights reserved.
+ * @license    MIT license; see LICENSE
+ */
+
+namespace IX\ScaleManager;
+
+/**
+ * Class Rule
+ */
+class Rule
+{
+	/**
+	 * The type of the state
+	 *
+	 * @var  string
+	 */
+	private $type;
+
+	private $value;
+
+	/**
+	 * The state value
+	 *
+	 * @var mixed
+	 */
+
+	/**
+	 * The comparision operator - $testValue $this->operator $this->value
+	 *
+	 * @var string
+	 */
+	private $operator;
+
+	/**
+	 * Minimum duration of the state needed to trogger the rule
+	 *
+	 * @var int Number of seconds
+	 */
+	private $duration;
+
+	/**
+	 * The command to be issued
+	 *
+	 * @var CommandInterface
+	 */
+	private $command;
+
+	/**
+	 * Rule constructor.
+	 *
+	 * @param string           $type     The state type
+	 * @param mixed            $value    The state threshold value
+	 * @param string           $operator The comparision operator
+	 * @param int              $duration The expected duration
+	 * @param CommandInterface $command  The command to be issued
+	 */
+	public function __construct($type, $value, $operator, $duration, CommandInterface $command)
+	{
+		$this->type     = $type;
+		$this->value    = $value;
+		$this->operator = $operator;
+		$this->duration = $duration;
+		$this->command  = $command;
+	}
+
+	/**
+	 * Get the state type
+	 *
+	 * @return string
+	 */
+	public function getType()
+	{
+		return $this->type;
+	}
+
+	/**
+	 * Get the state value
+	 *
+	 * @return mixed
+	 */
+	public function getValue()
+	{
+		return $this->value;
+	}
+
+	/**
+	 * Get the comparision operator
+	 *
+	 * @return string
+	 */
+	public function getOperator()
+	{
+		return $this->operator;
+	}
+
+	/**
+	 * Get the expected continuance
+	 *
+	 * @return int
+	 */
+	public function getDuration()
+	{
+		return $this->duration;
+	}
+
+	/**
+	 * Get the command
+	 *
+	 * @return CommandInterface
+	 */
+	public function getCommand()
+	{
+		return $this->command;
+	}
+}
