abc2522e8ebeebf5ded99f10a543c50fae8977a7
Tobias Weichart <tobias.weichart@gmail.com>
2017-03-26 19:04:46 +0200

    changes for testing

--- src/Collector.php
+++ src/Collector.php
@@ -2,6 +2,25 @@
 
 namespace IX\ScaleManager;
 
+use Slim\Http\Request;
+
 class Collector{
+
+    private $history;
+
+    public function __construct(History $history){
+        $this->history = $history;
+    }
+
+    public function collect(Request $request): void{
+        $payload = $this->getPayload($request);
+        $state = new State($payload->instance, $payload->type, $payload->value, $payload->timestamp);
+        $this->history->saveEvent($state);
+    }
+
+    private function getPayload(Request $request): \stdClass
+    {
+        return json_decode($request->getBody());
+    }
 
 }
--- tests/unit/CollectorTest.php
+++ tests/unit/CollectorTest.php
@@ -1,14 +1,28 @@
 <?php
 
 use IX\ScaleManager\Collector;
+use IX\ScaleManager\History;
+use Slim\Http\Request;
 
 class CollectorTest extends \PHPUnit\Framework\TestCase{
 
-    public function testCollector(){
-        $collector = new Collector();
-        $request = new HttpRequest();
+    public function testCollect(){
+        $history = $this->createMock(History::class);
+        $history
+            ->expects($this->once())
+            ->method('saveEvent');
+        $collector = new Collector($history);
+        $request = $this->createMock(Request::class);
+        $request
+            ->expects($this->any())
+            ->method('getBody')
+            ->willReturn(json_encode([
+                'instance' => 'reseller/123',
+                'type' => 'memory',
+                'value' => '20',
+                'timestamp' => '123123123'
+        ]));
         $collector->collect($request);
-        $this->asserTrue(true);
     }
 
 }
