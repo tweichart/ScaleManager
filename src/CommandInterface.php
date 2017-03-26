332b3971fd0aa006f88ab0c7b5c30f06bdd1cdb9
nibra <nbraczek@bsds.de>
2017-03-26 11:36:17 +0200

    CommandInterface

---
+++ src/CommandInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Part of the InterNetX ScaleManager
+ *
+ * @copyright  Copyright (C) 2017 InterNetX GmbH. All rights reserved.
+ * @license    MIT license; see LICENSE
+ */
+
+/**
+ * Interface CommandInterface
+ */
+interface CommandInterface
+{
+	/**
+	 * Get the instance ID
+	 *
+	 * @return string The instance ID
+	 */
+	public function getInstance();
+}
