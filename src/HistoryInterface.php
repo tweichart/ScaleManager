a0a9f42de7e2a6428844ed0a33a7ad540a267864
nibra <nbraczek@bsds.de>
2017-03-26 19:42:40 +0200

    Style changes

--- src/HistoryInterface.php
+++ src/HistoryInterface.php
@@ -11,27 +11,25 @@ namespace IX\ScaleManager;
 /**
  * Interface HistoryInterface
  */
-interface HistoryInterface {
+interface HistoryInterface
+{
 
-    /**
-     * Save an event to the history storage.
-     *
-     * @param \IX\ScaleManager\State $state
-     *  The state we want to store.
-     *
-     * @return bool
-     */
-    function saveEvent(State $state): bool;
+	/**
+	 * Save an event to the history storage.
+	 *
+	 * @param State $state The state we want to store.
+	 *
+	 * @return bool
+	 */
+	public function saveEvent(State $state): bool;
 
-    /**
-     * Fire a query against the Event Log and return a bool.
-     * The query is a simple SQL string.
-     *
-     * @param $query
-     * Json that contains the data for the query.
-     *
-     * @return bool
-     * We return true or false wrapped in Json.
-     */
-    function queryEventLog(string $query): bool;
+	/**
+	 * Fire a query against the Event Log and return a bool.
+	 * The query is a simple SQL string.
+	 *
+	 * @param string $query JSON that contains the data for the query.
+	 *
+	 * @return bool True or false wrapped in JSON.
+	 */
+	public function queryEventLog(string $query): bool;
 }
